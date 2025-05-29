dc2810ad5a4c7c05eee60c4b30cbbfecf9fc1270
Matias Medina <matiasmedinamedina538@gmail.com>
2025-05-28 20:09:58 -0400

    Inicio

---
+++ EcoMarket/src/test/java/com/example/EcoMarket/EcoMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EcoMarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
